0c047860dae19d12892373673248f31a6429e6b1
Qixiny <3185835784@qq.com>
2022-09-12 18:46:01 +0800

    update documents

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,40 @@
 #[macro_export]
-/// Build a function from a dynamic link library
-/// lib_path: The path of dll
-/// fn_name: The function of dll's function
-/// call_name: The call function of fn_name
-/// (v: t): The params of the function
+/// ## Get a function from a dynamic link library
+///
+/// * `lib_path`: the path of DLL
+/// * `fn_name`: The function name from dll
+/// * `call_name`: The call function name of `fn_name`
+/// * `ret`: return type of the function **if the function don't have return value, use "()" instead**
+/// * `(value: type)`: **(variadic argument)** The arguments of the function from dll
+///
+/// ## Example
+/// ```rust
+/// get_libfn!("libstd.dylib", "println", my_println, (), str: &str);
+/// my_println("Hello World");
+///
+/// get_libfn!("libstd.dylib", "add", my_add, usize, a: usize, b: usize);
+/// println!("10 + 20 = {}", my_add(10, 20));
+///
+/// get_libfn!("libstd.dylib", "print_hello", my_print_hello, ());
+/// my_print_hello();
+/// ```
+/// ### the contents of libstd.dylib:
+/// ```rust
+/// #[no_mangle]
+/// pub fn println(str: &str) {
+///     println!("{}", str);
+/// }
+/// #[no_mangle]
+/// pub fn add(a: usize, b: usize) -> usize {
+///     a + b
+/// }
+/// #[no_mangle]
+/// pub fn print_hello() {
+///     println!("Hello");
+/// }
+/// ```
 macro_rules! get_libfn {
-    ($lib_path: expr, $fn_name: expr, $call_name:ident, $ret: ty, $($v: ident: $t:ty),*) => {
+    ($lib_path: expr, $fn_name: expr, $call_name: ident, $ret: ty, $($v: ident: $t:ty),*) => {
         pub fn $call_name($($v: $t),*) -> $ret {
             unsafe {
                 let lib = libloading::Library::new("libstd.dylib").unwrap();
